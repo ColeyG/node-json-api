526df25db1906c7ef778947e2e99ee7d2cd48193
ColeyG <cjgeerts@gmail.com>
2019-11-20 01:17:02 -0500

    Init

---
+++ server.js
@@ -0,0 +1,33 @@
+const http = require('http');
+const express = require('express');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const path = require('path');
+
+const app = express();
+const httpServer = http.createServer(app);
+const config = require('./config.json');
+
+const PORT = process.env.PORT || 3000;
+
+httpServer.listen(3000, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
+
+app.use(session({ secret: config.sessionSecret, saveUninitialized: true, resave: true }));
+app.use(cookieParser());
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+const indexRouter = require('./routes/index');
+// const usersRouter = require('./routes/users');
+
+app.use('/', indexRouter);
+// app.use('/users', usersRouter);
